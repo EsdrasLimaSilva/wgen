bf476bf0fb6e0ff8370dcb6f2b431c955354d1a9
Esdras Silva <esdraslimasilva83@gmail.com>
2024-06-14 17:45:14 -0300

    Remove .env file

--- main.tsx
+++ main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./app";
-import ThemeContext from "./src/contexts/ThemeContext";
 
 const node = document.getElementById("root")!;
 const root = createRoot(node);
